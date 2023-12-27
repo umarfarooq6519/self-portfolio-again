ef086194ea18c17f24402e11b6d2f87d277c52cc
Umar Farooq <149324609+umarfarooq6519@users.noreply.github.com>
2023-12-27 23:21:07 +0500

    add src folder

---
+++ src/app.js
@@ -0,0 +1,10 @@
+//styling buttons
+const button_light = document.querySelectorAll("#button-light")
+button_light.forEach(button => {
+    button.classList.add("border", "border-[#352F44]", "px-4", "py-2", "rounded-lg", "hover:bg-[#352F44]", "hover:text-[#F4EAE0]", "transition", "shadow-lg");
+});
+
+const button_dark = document.querySelectorAll("#button-dark")
+button_dark.forEach(button => {
+    button.classList.add("border", "border-[#352F44]", "px-4", "py-2", "rounded-lg", "bg-[#352F44]", "text-[#F4EAE0]", "transition", "hover:bg-[#F4EAE0]", "hover:text-[#352F44]", "shadow-lg");
+});
